1a7e91a65bab050dbece7b883b45b945ca67f74d
JAbozarOid <araghibdoust@hotmail.com>
2021-05-21 16:00:09 +0430

    dynamic_stack_completed

--- lib/first/match_cards.dart
+++ lib/first/match_cards.dart
@@ -1,13 +1,19 @@
 class MatchCards {
-  int redColor = 0;
-  int greenColor = 0;
-  int blueColor = 0;
-  int margin = 0;
+  //int redColor = 0;
+  //int greenColor = 0;
+  //int blueColor = 0;
+  String imgStr;
+  double width;
+  double height;
+  double margin = 0;
 
-  MatchCards(int red, int green, int blue, int marginTop) {
-    this.redColor = red;
-    this.greenColor = greenColor;
-    this.blueColor = blueColor;
+  MatchCards(String imgUrl, double x, double y, double marginTop) {
+    //this.redColor = red;
+    //this.greenColor = greenColor;
+    //this.blueColor = blueColor;
+    this.imgStr = imgUrl;
+    this.width = x;
+    this.height = y;
     this.margin = marginTop;
   }
 }
--- lib/first/purposeful_messaging_screen.dart
+++ lib/first/purposeful_messaging_screen.dart
@@ -2,117 +2,105 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:profile/first/match_cards.dart';
 
-class PurposefulMessagingScreen extends StatelessWidget {
+class PurposefulMessagingScreen extends StatefulWidget {
+  @override
+  _PurposefulMessagingScreenState createState() =>
+      _PurposefulMessagingScreenState();
+}
+
+class _PurposefulMessagingScreenState extends State<PurposefulMessagingScreen> {
+  List<Widget> cardList;
+
+  void _removeCard(index) {
+    setState(() {
+      cardList.removeAt(index);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    cardList = _getMatchCards();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("the width is ${MediaQuery.of(context).size.width}");
+    print("the height is ${MediaQuery.of(context).size.height}");
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.arrow_back),
-        title: Text('PurposefulMessaging'),
+        title: Text('Purposeful Messaging'),
       ),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Positioned(
-              top: 10,
-              child: Card(
-                elevation: 4,
-                color: Color.fromARGB(255, 0, 0, 255),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: Container(
-                  width: 200,
-                  height: 300,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 20,
-              child: Card(
-                elevation: 8,
-                color: Color.fromARGB(255, 0, 255, 0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Container(
-                  width: 220,
-                  height: 300,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 30,
-              child: Draggable(
-                childWhenDragging: Container(),
-                feedback: Card(
-                  elevation: 12,
-                  color: Color.fromARGB(255, 200, 0, 0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Container(
-                    width: 240,
-                    height: 300,
-                  ),
-                ),
-                child: Card(
-                  elevation: 12,
-                  color: Color.fromARGB(255, 200, 0, 0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Container(
-                    width: 240,
-                    height: 300,
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      body: Stack(alignment: Alignment.center, children: cardList),
     );
   }
 
   List<Widget> _getMatchCards() {
     List<MatchCards> cards = <MatchCards>[];
-    cards.add(MatchCards(255, 0, 0, 10));
-    cards.add(MatchCards(0, 255, 0, 20));
-    cards.add(MatchCards(0, 0, 255, 30));
+    cards.add(MatchCards('assets/abozar3.png', 310, 520, 30));
+    cards.add(MatchCards('assets/abozar2.jpg', 330, 510, 20));
+    cards.add(MatchCards('assets/abozar1.png', 350, 500, 10));
 
     List<Widget> cardList = [];
     for (int x = 0; x < 3; x++) {
-      cardList.add(Positioned(
-        top: 30,
-        child: Draggable(
-          childWhenDragging: Container(),
-          feedback: Card(
-            elevation: 12,
-            color: Color.fromARGB(255, cards[x].redColor, cards[x].greenColor,
-                cards[x].blueColor),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+      cardList.add(
+        Positioned(
+          top: cards[x].margin,
+          child: Draggable(
+            onDragEnd: (drag) {
+              _removeCard(x);
+            },
+            childWhenDragging: Container(),
+            feedback: SizedBox(
+              width: cards[x].width,
+              height: cards[x].height,
+              child: Card(
+                  elevation: 12,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Container(
+                    width: cards[x].width,
+                    height: cards[x].height,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: Image.asset(
+                              cards[x].imgStr,
+                              width: cards[x].width,
+                              height: cards[x].height,
+                            ).image,
+                            fit: BoxFit.cover)),
+                  )),
             ),
-            child: Container(
-              width: 240,
-              height: 300,
-            ),
-          ),
-          child: Card(
-            elevation: 12,
-            color: Color.fromARGB(255, cards[x].redColor, cards[x].greenColor,
-                cards[x].blueColor),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Container(
-              width: 240,
-              height: 300,
+            child: SizedBox(
+              width: cards[x].width,
+              height: cards[x].height,
+              child: Card(
+                elevation: 12,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Container(
+                  width: cards[x].width,
+                  height: cards[x].height,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          image: Image.asset(cards[x].imgStr,
+                                  width: cards[x].width,
+                                  height: cards[x].height)
+                              .image,
+                          fit: BoxFit.cover)),
+                ),
+              ),
             ),
           ),
         ),
-      ));
+      );
     }
+
+    return cardList;
   }
 }
